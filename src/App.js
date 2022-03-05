5aa87230ced872216386eb60bb2b4ecfbfc96712
DESKTOP-PATBRMT\Admin <estuardogomez1996@gmail.com>
2022-03-04 21:51:12 -0600

    Esta es una prueba

--- src/App.js
+++ src/App.js
@@ -9,6 +9,9 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <p>
+          Hola esta es mi primer app jajaja
+        </p>
         <a
           className="App-link"
           href="https://reactjs.org"
