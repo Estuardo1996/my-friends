db1d9709fef4b8a28fc9cff7ccd50bcb1a128879
DESKTOP-PATBRMT\Admin <estuardogomez1996@gmail.com>
2022-03-08 22:02:52 -0600

    Se aprendieron nuevas cosas

--- src/App.js
+++ src/App.js
@@ -1,6 +1,10 @@
 import logo from './logo.svg';
 import './App.css';
 import Welcome from './components/Welcome';
+import {BrowserRouter as Router, Routes, Route, Link} from 'react-router-dom'
+import Contact from './Pages/Contact';
+import AboutMe from './Pages/AboutMe';
+import Profile from './Pages/Profile';
 
 function App() {
   return (
@@ -8,6 +12,29 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <Welcome message= "Hola welcome props" name = "Gomez"/>
+        <Router>
+        <div> 
+          <ul>
+          <li>
+           <Link to="/">Home</Link> 
+          </li>  
+          <li>
+          <Link to="/contact">Contacto</Link> 
+          </li> 
+          <li>
+          <Link to="/about-me">Sobre Mi</Link> 
+          </li>
+          <li>
+          <Link to="/profile">Perfil</Link> 
+          </li>
+          </ul>
+        </div>
+          <Routes>
+            <Route path="/contact" element = {<Contact/>}/> 
+            <Route path="/about-me" element = {<AboutMe/>}/> 
+            <Route path="/profile/:name" element = {<Profile/>}/> 
+          </Routes> 
+        </Router>
       </header>
     </div>
   );
---
+++ src/Pages/AboutMe.js
@@ -0,0 +1,7 @@
+export default function AboutMe(){
+    return(
+        <div>
+            <h4> Desde Sobre Mi</h4>
+        </div>
+    )
+}
